6c24c0b5237fd11eb5e4b6ed2b0f48bd2a8b0023
Wesley Roekens <Roekenswesley@gmail.com>
2024-10-17 19:18:55 +0200

    Changes

--- routes/v1/api/messages.js
+++ routes/v1/api/messages.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const messageController = require('../../../controllers/api/v1/messages');
+const messageController = require('../../../controllers/api/v1/message');
 
 router.post('/', messageController.create);
 router.get('/', messageController.index);

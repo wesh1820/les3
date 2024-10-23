20d528e510571f23bfce608bdacb083415aea172
Wesley Roekens <Roekenswesley@gmail.com>
2024-10-23 10:49:07 +0200

    Challenge4

---
+++ app.vue
@@ -0,0 +1,29 @@
+<!-- src/App.vue -->
+<template>
+  <div id="app">
+    <h1>TikTok Demo</h1>
+    <Videos />
+    <Chat />
+  </div>
+</template>
+
+<script>
+import Videos from './components/Videos.vue';
+import Chat from './components/Chat.vue';
+
+export default {
+  components: {
+    Videos,
+    Chat,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
